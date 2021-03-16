9e3046f4ae025d07164d8b3dfa0366c2e2a7a779
lucyk22 <71743901+lucyk22@users.noreply.github.com>
2021-03-16 02:46:47 -0400

    setup, started js

---
+++ src/js/_arrays.js
@@ -0,0 +1,29 @@
+const questions = [
+    { question: "The following are all Mondstadt characters except", answers: [
+        { text:'Venti', correct:false}
+        { text:'Mona', correct:false}
+        { text:'Fischl', correct:false}
+        { text:'Cyno', correct:true}
+    ]}
+]
+
+   /* except", answer: 4 },
+    { question: "What is the Vigilant Yaksha's true name?", answer: 2 },
+    { question: "Diluc, owner of the Dawn Winery, prefers to drink", answer: 2 },
+    {
+      question: "Which of the following is NOT one of the four winds?",
+      answer: 4,
+    },
+    {
+      question: "During the Archon war, which god was slain by their own people?",
+      answer: 1,
+    },
+    { question: "What caused the fall of Sal Vindagnyr?", answer: 3 },
+    {
+      question: "When deciphered, in what language is the lost language written?",
+      answer: 3,
+    },
+    { question: "What secret is Celestia hiding from humanity?", answer: 2 },
+    { question: "What is the name of the Unknown God?", answer: 1 },
+    { question: "What are the stars?", answer: 1 },
+  ]; */
---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+//arrays
+const questions = [
+  { question: "The following are all Mondstadt characters except", answer: 4 },
+  { question: "What is the Vigilant Yaksha's true name?", answer: 2 },
+  { question: "Diluc, owner of the Dawn Winery, prefers to drink", answer: 2 },
+  {
+    question: "Which of the following is NOT one of the four winds?",
+    answer: 4,
+  },
+  {
+    question: "During the Archon war, which god was slain by their own people?",
+    answer: 1,
+  },
+  { question: "What caused the fall of Sal Vindagnyr?", answer: 3 },
+  {
+    question: "When deciphered, in what language is the lost language written?",
+    answer: 3,
+  },
+  { question: "What secret is Celestia hiding from humanity?", answer: 2 },
+  { question: "What is the name of the Unknown God?", answer: 1 },
+  { question: "What are the stars?", answer: 1 },
+];
+
+//other constants
+//const chOne = document.querySelector("#choice1");
+
+//not the constants
+
+//testing functions
+//chOne.addEventListener("click", pressButtonOne);
+
+//function pressButtonOne() {
+//  console.log("pressed button one");
+
+//real deal
+
+//function replaceQuestion() {}
+
+//function replaceAnswer() {}
+
+const chOne = document.getElementById("ch1");
+chOne.potato = ["lemons", 3];
